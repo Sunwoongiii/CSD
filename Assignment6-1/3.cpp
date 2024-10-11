dcf8fcd16f4aabbc1ccd9a5d302e9b8c8d76a4cc
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-10-11 16:59:38 +0900

    modified:   Assignment6-1/3.cpp

--- Assignment6-1/3.cpp
+++ Assignment6-1/3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Printer{
 private:
-  int availableCount; //ink or toner capacity
+  int availableCount;
   int availablePage;
 public:
   Printer(int ac, int ap): availableCount(ac), availablePage(ap){}
@@ -19,41 +19,45 @@ public:
   }
 };
 
-class InkjetP : public Printer{//ac == ink
+class InkjetP : public Printer{
 private:
-  const string model = "Officejet V30";
-  const string manufacturer = "HP";
+  string model;
+  string manufacturer;
 public:
-  InkjetP(int ac, int ap): Printer(ac,ap){}
+  InkjetP(int ac, int ap, string model, string manufacturer): Printer(ac,ap), model(model), manufacturer(manufacturer) {}
   void print(int usedPage){
-    if(usedPage > getAvailableCount())cout<<"Cannot print because of not enough ink.\n";
-    else if(usedPage > getAvailablePage())cout<<"Cannot print because of not enough page.\n";
+    if(usedPage > getAvailableCount()){
+      cout<<"Cannot print because of not enough ink.\n";
+    }
+    else if(usedPage > getAvailablePage()){
+      cout<<"Connot print because of not enough pages.\n";
+    }
     else{
-      cout<<"Printed.\n";
       Printer::print(usedPage);
+      cout<<"Printed.\n";
     }
   }
   void showInfo(){
-    cout<<"InkJet: "<<model<<", "<<manufacturer<<", "<<Printer::getAvailablePage()<<" available pages, "<<Printer::getAvailableCount()<<" available Ink\n";
+    cout<<"InkJet: "<<model<<", "<<manufacturer<<", "<<getAvailablePage()<<" available paged, "<<getAvailableCount()<<" available Ink\n";
   }
 };
 
-class Laser : public Printer{
+class LaserP : public Printer{
 private:
-  const string model = "SCX-6x47";
-  const string manufacturer = "Samsung";
+  string model;
+  string manufacturer;
 public:
-  Laser(int ac, int ap): Printer(ac,ap){}
+  LaserP(int ac, int ap, const string &model, const string &manufacturer): Printer(ac, ap),model(model), manufacturer(manufacturer){}
   void print(int usedPage){
-    if(usedPage > getAvailableCount())cout<<"Cannot print because of not enought toner.\n";
-    else if(usedPage > getAvailablePage())cout<<"Cannot print because of not enough page.\n";
+    if(usedPage > getAvailableCount())cout<<"Cannot print because of not enough toner.\n";
+    else if(usedPage > getAvailablePage())cout<<"Cannot print because of not enough pages.\n";
     else{
-      cout<<"Printed.\n";
       Printer::print(usedPage);
+      cout<<"Printed.\n";
     }
   }
   void showInfo(){
-    cout<<"Laser: "<<model<<", "<<manufacturer<<", "<<Printer::getAvailablePage()<<" available pages, "<<Printer::getAvailableCount()<<" available Toner\n";
+    cout<<"Laser: "<<model<<", "<<manufacturer<<", "<<getAvailablePage()<<" available pages, "<<getAvailableCount()<<" available toner\n";
   }
 };
 
@@ -61,41 +65,32 @@ int main(){
   cout<<"Currently operating 2 printers are follow\n";
   int ac, ap;
   cin>>ap>>ac;
-  InkjetP inkPrinter(ac,ap);
-  inkPrinter.showInfo();
+  InkjetP ink(ac, ap, "Officejet V30", "HP");
+  ink.showInfo();
   cin>>ap>>ac;
-  Laser tonerPrinter(ac,ap);
-  tonerPrinter.showInfo();
-  cout<<"\n";
+  LaserP toner(ac,ap,"SCX-x47", "Samsung");
+  toner.showInfo();
 
   while(true){
     int command, pages;
-    string continous;
+    string con;
+    cout<<"\n";
     cout<<"Insert printer(1:InkJet, 2:Laser) and pages: ";
     cin>>command>>pages;
+
     if(command == 1){
-      inkPrinter.print(pages);
-      inkPrinter.showInfo();
-      tonerPrinter.showInfo();
-      cout<<"Do you want keep printing?(y/n)>>";
-      cin>>continous;
-      if(continous == "y" || continous == "Y"){
-        cout<<"\n";
-        continue;
-      }
-      else return 0;
+      ink.print(pages);
+      ink.showInfo();
+      toner.showInfo();
     }
     else if(command == 2){
-      tonerPrinter.print(pages);
-      inkPrinter.showInfo();
-      tonerPrinter.showInfo();
-      cout<<"Do you want keep printing?(y/n)>>";
-      cin>>continous;
-      if(continous == "y"||continous=="Y"){
-        cout<<"\n";
-        continue;
-      }
-      else return 0;
+      toner.print(pages);
+      ink.showInfo();
+      toner.showInfo();
     }
+    cout<<"Do you want keep printing?(y/n)>>";
+    cin>>con;
+    if(con == "y" || con == "Y") continue;
+    else return 0;
   }
 }
