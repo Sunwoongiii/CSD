040a6c8132edf213091c44b2a9e4e2489014ea9d
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-11-20 16:08:35 +0900

    modified:   Assignment12-2/1.cpp 	modified:   Assignment12-2/1.exe 	new file:   Assignment12-2/13178_Assignment12-2_2024017201.zip 	modified:   Assignment12-2/2.cpp 	new file:   Assignment13-1/.vscode/c_cpp_properties.json 	new file:   Assignment13-1/.vscode/launch.json 	new file:   Assignment13-1/.vscode/settings.json 	new file:   Assignment13-1/.vscode/tasks.json 	new file:   Assignment13-1/1.cpp 	new file:   Assignment13-1/1.exe 	new file:   Assignment13-1/2.cpp 	new file:   Assignment13-1/2.exe 	renamed:    Assignment12-2/3.cpp -> Assignment13-1/3.cpp 	renamed:    Assignment12-2/3.exe -> Assignment13-1/3.exe 	new file:   Assignment13-1/Assignment_13-1.pdf

--- Assignment12-2/2.cpp
+++ Assignment12-2/2.cpp
@@ -1,42 +1,67 @@
 #include <iostream>
-using namespace std;
+#include <array>
+#include <vector>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <cmath>
+#include <algorithm>
 
-class MyException {
-public:
-  void report() {
-    cout << "Exception report" << endl;
-  }
-};
-
-class MyArray {
-private:
-  int* my_array;
-  int maxSize;
-
-public:
-  MyArray(int size) : maxSize(size) {
-    my_array = new int[maxSize];
-  }
-
-  ~MyArray() {
-    delete[] my_array;
-  }
-
-  int& operator[](const int& index) {
-    if (index < 0 || index >= maxSize) {
-      throw MyException();
+template <typename T>
+void print_stats(const T& container) {
+    if (container.empty()) {
+        std::cout << "Container is empty." << std::endl;
+        return;
     }
-    return my_array[index];
-  }
-};
+
+    double sum = 0;
+    double min_val = *std::min_element(container.begin(), container.end());
+    double max_val = *std::max_element(container.begin(), container.end());
+    std::vector<typename T::value_type> sorted_container(container.begin(), container.end());
+    std::sort(sorted_container.begin(), sorted_container.end());
+
+    double median = 0;
+    if (sorted_container.size() % 2 == 0) {
+        median = (sorted_container[sorted_container.size() / 2 - 1] + sorted_container[sorted_container.size() / 2]) / 2.0;
+    } else {
+        median = sorted_container[sorted_container.size() / 2];
+    }
+
+    for (const auto& num : container) {
+        sum += num;
+    }
+    double average = sum / container.size();
+
+    double variance_sum = 0;
+    for (const auto& num : container) {
+        variance_sum += std::pow(num - average, 2);
+    }
+    double variance = variance_sum / container.size();
+    double std_dev = std::sqrt(variance);
+
+    std::cout << "Minimum: " << min_val << std::endl;
+    std::cout << "Maximum: " << max_val << std::endl;
+    std::cout << "Median: " << median << std::endl;
+    std::cout << "Average: " << average << std::endl;
+    std::cout << "Variance: " << variance << std::endl;
+    std::cout << "Standard Deviation: " << std_dev << std::endl;
+}
 
 int main() {
-  MyArray myArray(5);
-  try {
-    myArray[20];
-  } catch (MyException& e) {
-    e.report();
-  }
+    std::vector<int> vec = {5, 3, 9, 1, 6};
+    print_stats(vec);
 
-  return 0;
+    std::list<double> lst = {2.5, 3.5, 1.5, 4.5};
+    print_stats(lst);
+
+    std::deque<int> deq = {7, 2, 8, 3, 5};
+    print_stats(deq);
+
+    std::array<int, 4> arr = {1, 4, 2, 3};
+    print_stats(arr);
+
+    std::forward_list<int> flist = {11, 7, 13, 9};
+    print_stats(flist);
+
+    return 0;
 }
