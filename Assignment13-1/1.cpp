6e4bde90f0e5590deee81bb7c3a219e582f99bf6
Sunwoongiii <its.sunwoongiii@gmail.com>
2024-12-09 15:41:39 +0900

    modified:   Assignment13-1/1.cpp 	modified:   Assignment13-1/1.exe 	modified:   Assignment13-1/2.cpp 	modified:   Assignment13-1/2.exe 	renamed:    Assignment13-1/Assignment_13-2.pdf -> Assignment13-2/Assignment_13-2.pdf

--- Assignment13-1/1.cpp
+++ Assignment13-1/1.cpp
@@ -1,100 +1,31 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-class Container {
+int add_int(const int a, const int b){return a+b;}
+double add_double(const double a, const double b){return a+b;}
+
+template<typename T, typename U>
+auto add_any(const T& a, const U& b)->decltype(a+b){
+  static_assert(std::is_same<decltype(a+b), decltype(a+b)>::value, "Types must support addition operator.\n");
+  return a+b;
+}
+
+class MyClass{
 public:
-  virtual void push_back(int value) = 0;
-  virtual void pop_back() = 0;
-  virtual int getVectorArr(int index) = 0;
-  virtual void setVectorArr(int index, int value) = 0;
-  virtual ~Container() {}
+  int value;
+  MyClass(int v):value(v){}
+  MyClass operator+(const MyClass& other)const{
+    return MyClass(value + other.value);
+  }
+  void print()const{cout<<value<<'\n';}
 };
 
-class MyVector : public Container {
-private:
-  int* data;
-  int size;
-  int capacity;
+int main(){
+  MyClass o1(10), o2(20);
+  MyClass o3 = add_any(o1, o2);
 
-  void resize() {
-    capacity *= 2;
-    int* newData = new int[capacity];
-    for (int i = 0; i < size; i++) {
-      newData[i] = data[i];
-    }
-    delete[] data;
-    data = newData;
-  }
-
-public:
-  MyVector(int initialCapacity) : size(0), capacity(initialCapacity) {
-    data = new int[capacity];
-  }
-
-  ~MyVector() {
-    delete[] data;
-  }
-
-  void push_back(int value) override {
-    if (size == capacity) {
-      resize();
-    }
-    data[size++] = value;
-  }
-
-  void pop_back() override {
-    if (size == 0) {
-      throw "nothing to pop";
-    }
-    size--;
-  }
-
-  int getVectorArr(int index) override {
-    if (index < 0 || index >= size) {
-      throw "Out of range error";
-    }
-    return data[index];
-  }
-
-  void setVectorArr(int index, int value) override {
-    if (index < 0 || index >= size) {
-      throw "Out of range error";
-    }
-    data[index] = value;
-  }
-};
-
-int main() {
-  Container* v = new MyVector(5);
-
-  for (int i = 0; i < 10; i++) {
-    v->push_back(i * 10);
-  }
-
-  try {
-    v->setVectorArr(11, 20); 
-  } catch (const char* msg) {
-    cerr << msg << endl;
-  }
-
-  for (int i = 9; i >= 0; i--) {
-    try {
-      cout << v->getVectorArr(i) << " ";
-    } catch (const char* msg) {
-      cerr << msg << endl;
-    }
-  }
-  cout << endl;
-
-  for (int j = 0; j < 11; j++) {
-    try {
-      v->pop_back();
-    } catch (const char* msg) {
-      cerr << msg << endl;
-    }
-  }
-
-  delete v;
-  return 0;
+  cout<<"add_int(3,4): "<<add_int(3,4)<<'\n';
+  cout<<"add_double(3.1,4.2): "<<add_double(3.1,4.2)<<'\n';
+  cout<<"add_any(string, string): "<<add_any(string("Hello "), string("World!"))<<'\n';
+  cout<<"add_any(class, class): "; o3.print();
 }
--- Assignment13-1/2.cpp
+++ Assignment13-1/2.cpp
@@ -1,42 +1,87 @@
 #include <iostream>
+#include <vector>
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
 using namespace std;
 
-class MyException {
-public:
-  void report() {
-    cout << "Exception report" << endl;
-  }
-};
+template<typename Container>
+double calculate_median(Container container){
+  using ValueType = typename Container::value_type;
+  vector<ValueType> sorted_data(container.begin(), container.end());
+  sort(sorted_data.begin(), sorted_data.end());
 
-class MyArray {
-private:
-  int* my_array;
-  int maxSize;
+  size_t n = sorted_data.size();
+  if(n%2 == 0) return (sorted_data[n/2-1]+sorted_data[n/2])/2.0;
+  else return sorted_data[n/2];
+}
 
-public:
-  MyArray(int size) : maxSize(size) {
-    my_array = new int[maxSize];
+template<>
+double calculate_median<forward_list<double>>(forward_list<double> container){
+  vector<double> sorted_data(container.begin(), container.end());
+  sort(sorted_data.begin(), sorted_data.end());
+
+  size_t n = sorted_data.size();
+  if(n%2 == 0)return (sorted_data[n/2-1]+sorted_data[n/2])/2.0;
+  else return sorted_data[n/2];
+}
+
+template <typename Container>
+void print_stats(const Container& container){
+  static_assert(is_arithmetic<typename Container::value_type>::value, "Container must hold numeric values.\n");
+  
+  if(container.empty()){
+    cout<<"Container is empty. No statistics available.\n";
+    return;
   }
 
-  ~MyArray() {
-    delete[] my_array;
+  using ValueType = typename Container::value_type;
+  ValueType min_value = numeric_limits<ValueType>::max();
+  ValueType max_value = numeric_limits<ValueType>::lowest();
+
+  double sum = 0.0;
+  double sum_of_squares = 0.0;
+  size_t cnt = 0;
+
+  for(const auto& value : container){
+    if(value < min_value)min_value = value;
+    if(value > max_value)max_value = value;
+    sum += value;
+    sum_of_squares += value*value;
+    ++cnt;
   }
 
-  int& operator[](const int& index) {
-    if (index < 0 || index >= maxSize) {
-      throw MyException();
-    }
-    return my_array[index];
-  }
-};
+  double mean = sum / cnt;
+  double variance = sum_of_squares/cnt - mean*mean;
+  double std_dev = sqrt(variance);
+  double median = calculate_median(container);
 
-int main() {
-  MyArray myArray(5);
-  try {
-    myArray[20];
-  } catch (MyException& e) {
-    e.report();
-  }
+  cout<<"Minimum: "<<min_value<<'\n';
+  cout<<"Maximum: "<<max_value<<'\n';
+  cout<<"Median: "<<median<<'\n';
+  cout<<"Average: "<<mean<<'\n';
+  cout<<"Variance: "<<variance<<'\n';
+  cout<<"Standard Deviation: "<<std_dev<<'\n';
+}
+
+int main(){
+  vector<double> vec = {1.2,3.4,2.5,7.8,5.6};
+  deque<int>deq = {10,20,30,40,50};
+  list<float> lst = {2.1f, 4.3f, 3.3f, 5.7f};
+  forward_list<int>fwd_lst = {9,2,7,4,6};
+
+  cout<<"Statistics for vector: \n";
+  print_stats(vec);
+  cout<<"\nStatistics for deque: \n";
+  print_stats(deq);
+  cout<<"\nStatistics for list:\n";
+  print_stats(lst);
+  cout<<"\nStatistics for forward_list:\n";
+  print_stats(fwd_lst);
 
   return 0;
 }
